30a130627e72c1c303b1b92dd9e911dc48e06f2a
Your Name <you@example.com>
2021-12-11 11:53:54 +0100

    date refactor

---
+++ LAB2/script.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', function () {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; 
+    var yyyy = today.getFullYear();
+
+    var day = today.getDay();
+    if(day === 0){
+        dd = dd + 1;
+    }else if(day === 6){
+        dd = dd + 2;
+    }
+    
+    if (dd < 10) {
+       dd = '0' + dd;
+    }
+    
+    if (mm < 10) {
+       mm = '0' + mm;
+    } 
+        
+    today = yyyy + '-' + mm + '-' + dd;
+    document.getElementById("datefield").setAttribute("min", today);
+})
+
