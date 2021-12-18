80b7ae9a5870a9d86160fe23265d6a5938cbe126
Damian Brzeziński <209278@edu.p.lodz.pl>
2021-12-18 14:04:13 +0100

    added drag and drop

--- LAB3/zadanie2/script.js
+++ LAB3/zadanie2/script.js
@@ -1,1 +1,36 @@
+let itemId;
+let canGenerateNew = false;
+let newId = 2;
 
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+  
+function drag(ev) {
+    itemId = ev.target.id;
+}
+
+function drop(ev) {
+    if(itemId && ev.target.className != "item field notADropZone"){
+        ev.preventDefault();
+        ev.target.appendChild(document.getElementById(itemId));
+        document.getElementById(itemId).toggleAttribute("draggable");
+        document.getElementById(itemId).toggleAttribute("ondrag");
+        itemId = undefined;
+        canGenerateNew = true;
+    }
+}
+
+function myClick(ev){
+    if(canGenerateNew){
+        var newElement = document.createElement("canvas");
+        newElement.setAttribute("id", newId);
+        newId++;
+        newElement.setAttribute("class", "item field notADropZone");
+        newElement.setAttribute("ondrag", "drag(event)");
+        newElement.setAttribute("draggable", "true");
+
+        document.getElementsByClassName("footer")[0].appendChild(newElement);
+        canGenerateNew = false;
+    }
+}
