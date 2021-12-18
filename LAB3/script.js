315a0c752eecf8a1cee9400d815dbf8dd207ae1c
Damian Brzeziński <209278@edu.p.lodz.pl>
2021-12-18 11:51:13 +0100

    refactor for functions

--- LAB3/script.js
+++ LAB3/script.js
@@ -4,34 +4,34 @@ let index;
 let indexDrop;
 let list;
 
-  document.addEventListener("dragstart", ({target}) => {
-      dragged = target;
-      id = target.id;
-      list = target.parentNode.children;
-      for(let i = 0; i < list.length; i += 1) {
-        if(list[i] === dragged){
-          index = i;
+function drop(ev){
+    if(ev.target.className == "dropzone" && ev.target.id !== id) {
+        dragged.remove( dragged );
+        for(let i = 0; i < list.length; i += 1) {
+            if(list[i] === ev.target){
+            indexDrop = i;
+            }
         }
-      }
-  });
-
-  document.addEventListener("dragover", (event) => {
-      event.preventDefault();
-  });
-
-  document.addEventListener("drop", ({target}) => {
-   if(target.className == "dropzone" && target.id !== id) {
-       dragged.remove( dragged );
-      for(let i = 0; i < list.length; i += 1) {
-        if(list[i] === target){
-          indexDrop = i;
+        console.log(index, indexDrop);
+        if(index > indexDrop) {
+            ev.target.before( dragged );
+        } else {
+            ev.target.after( dragged );
         }
-      }
-      console.log(index, indexDrop);
-      if(index > indexDrop) {
-        target.before( dragged );
-      } else {
-        target.after( dragged );
-      }
     }
-  });
+}
+
+function drag(ev){
+    dragged = ev.target;
+    id = ev.target.id;
+    list = ev.target.parentNode.children;
+    for(let i = 0; i < list.length; i += 1) {
+        if(list[i] === dragged){
+            index = i;
+        }
+    }
+};
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
